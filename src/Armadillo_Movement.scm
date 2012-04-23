06511dab5540b4b21738a744600f7b1d3bc5115d
alvatarc <alvcastro@yahoo.es>
2012-04-23 13:52:44 +0200

    Algorithm Ended. Constants to be Tested.

--- src/Armadillo_Movement.scm
+++ src/Armadillo_Movement.scm
@@ -1,33 +1,47 @@
+;;Sistema de Referencia: (maxx/2, maxy/2)
+
 ;;Acceleration
 
+;;Plain_Speed: 11.5 pixels/ms
+;;Max_Pendant: 1.22rads
+;;
+
+
 (define (acceleration angle)
-  (error "To Be Implemented: Speed Formula [(* 9.8 (sin angle)]"))
+  (* 3 (sin angle)))
 
 ;;Speed
 
-(define (speed current_speed max_speed min_speed acceleration side) 
-  (if (equal? side up)
-      (if (= current_speed min_speed)
+(define (speed current_speed max_speed min_speed plain_speed pendant) 
+  (cond ((equal? pendant null)
+         ((= current_speed plain_soeed)
           current_speed
-          (- current_speed (* acceleration 10000)))
-      (if (= current_speed max_speed)
+          ((< current_speed plain_speed)
+           (+ current_speed (* (acceleration angle) 10))
+           (- current_speed (* (acceleration angle) 10)))))
+         ((equal? pendant up)         
+         ((= current_speed min_speed)
+          current_speed
+          (- current_speed (* (acceleration angle) 10)))
+         ((= current_speed max_speed)
           speed
-          (+ current_speed (* acceleration 10000)))))
+          (+ current_speed (* (acceleration angle) 10))))))
+
 
 ;;Movement
 
-(define (horizontal_move current_speed initial_speed min_x max_x)
+(define (horizontal_move current_speed initial_speed current_pos min_x max_x)
   (cond ((= current_speed initial_speed)
          ((= armadillo-pos-x 0)
           armadillo-pos-x
           ((< 0 armadillo-pos-x)
-           (error "To Be Implemented: x-(v*t)")
-           (error "To Be Implemented: x+(v*t)"))))
+           (- current_pos (* current_speed 10)) 
+           (+ current_pos (* current_speed 10)))))
         ((< current_speed initial_speed)
          ((= armadillo-pos-x min_x)
           armadillo-pos-x
-          (error "To Be Implemented: x-(v*t)")))
+          (- current_pos (* current_speed 10)))
         ((> current_speed initial_speed)
          ((= armadillo-pos-x max_x)
           (armadillo_pos_x)
-          (error "To Be Implemented: x+(v*t)")))))
+          (+ current_pos (* current_speed 10))))))) 
