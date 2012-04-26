6fd1959b6bf92d28cd43d5b44bd51fcbb479fa9c
alvatarc <alvcastro@yahoo.es>
2012-04-26 13:46:32 +0200

    Final Version of Armadillo Movement Algorithm Ended

--- src/Armadillo_Movement.scm
+++ src/Armadillo_Movement.scm
@@ -6,42 +6,49 @@
 ;;Max_Pendant: 1.22rads
 ;;
 
+(define maxx 1280)
 
 (define (acceleration angle)
   (* 3 (sin angle)))
 
 ;;Speed
 
-(define (speed current_speed max_speed min_speed plain_speed pendant) 
-  (cond ((equal? pendant null)
-         ((= current_speed plain_soeed)
-          current_speed
-          ((< current_speed plain_speed)
-           (+ current_speed (* (acceleration angle) 10))
-           (- current_speed (* (acceleration angle) 10)))))
-         ((equal? pendant up)         
-         ((= current_speed min_speed)
-          current_speed
-          (- current_speed (* (acceleration angle) 10)))
-         ((= current_speed max_speed)
-          speed
-          (+ current_speed (* (acceleration angle) 10))))))
+(define (speed current_speed max_speed min_speed plain_speed pendant angle) 
+  (cond ((equal? pendant 'null)
+         (cond ((= current_speed plain_speed)
+                current_speed)                  
+               ((< current_speed plain_speed)
+                (+ current_speed (* (acceleration angle) 10)))
+               ((> current_speed plain_speed)
+                (- current_speed (* (acceleration angle) 10)))))
+        ((equal? pendant 'up)         
+         (if (= current_speed min_speed)
+             current_speed
+             (- current_speed (* (acceleration angle) 10))))
+        ((equal? pendant 'down)
+         (if (= current_speed max_speed)
+             current_speed                       
+             (+ current_speed (* (acceleration angle) 10))))))
 
 
 ;;Movement
 
 (define (horizontal_move current_speed initial_speed current_pos min_x max_x)
   (cond ((= current_speed initial_speed)
-         ((= armadillo-pos-x 0)
-          armadillo-pos-x
-          ((< 0 armadillo-pos-x)
-           (- current_pos (* current_speed 10)) 
-           (+ current_pos (* current_speed 10)))))
+         (cond ((= current_pos 0)
+                current_pos)
+               ((< 0 armadillo-pos-x)
+                (- current_pos (* current_speed 10)))
+               ((> 0 armadillo-pos-x)
+                (+ current_pos (* current_speed 10)))))
         ((< current_speed initial_speed)
-         ((= armadillo-pos-x min_x)
-          armadillo-pos-x
-          (- current_pos (* current_speed 10)))
+         (if (= current_pos min_x)
+             armadillo-pos-x
+             (- current_pos (* current_speed 10))))
         ((> current_speed initial_speed)
-         ((= armadillo-pos-x max_x)
-          (armadillo_pos_x)
-          (+ current_pos (* current_speed 10))))))) 
+         (if (= current_pos max_x)
+             (armadillo_pos_x)
+             (+ current_pos (* current_speed 10)))))) 
+
+(display (horizontal_move (speed 13.8 16.0 7.0 11.5 'up 0.8) 11.5 0 (* 0.2 maxx) (* 0.8 maxx)))
+(newline)
